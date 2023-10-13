80204abfcbb31831e3f7aed2740e4da43f734409
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2023-10-14 00:58:29 +0530

    GetElementsbyTAgName

--- app.js
+++ app.js
@@ -16,3 +16,20 @@ console.log(items[1]);
 for(var i = 0; i < items.length; i++){
   items[i].style.backgroundColor='#f4f4f4';
 }
+
+
+const newLi = document.createElement('li');
+newLi.textContent = 'New List Item'; 
+const ulElement = document.querySelector('.list-group');
+ulElement.appendChild(newLi);
+
+const elementsWithSameClass = document.getElementsByClassName('my-list');
+if (elementsWithSameClass.length > 0) {
+  elementsWithSameClass[0].textContent = 'Updated List Item (using getElementsByClassName)';
+}
+
+const elementsWithTagName = document.getElementsByTagName('li');
+if (elementsWithTagName.length > 0) {
+  elementsWithTagName[0].textContent = 'Updated List Item (using getElementsByTagName)';
+}
+
